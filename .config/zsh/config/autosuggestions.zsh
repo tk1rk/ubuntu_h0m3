c414cada0865db7e7d7b3d911d8aa2c4a4f94f7b
Taylor K. <tkirk85@gmail.com>
2025-03-27 23:27:15 -0700

    Create autosuggestions.zsh

---
+++ .config/zsh/config/autosuggestions.zsh
@@ -0,0 +1,5 @@
+# autosuggestions
+ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
+ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+ZSH_AUTOSUGGEST_CLEAR_WIDGETS=(expand-or-complete bracketed-paste accept-line push-line-or-edit)
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#ff79c6,bold"
